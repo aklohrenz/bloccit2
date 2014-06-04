4305645f5f20f3504e2bf4f5fac9a29ada0b1baf
Aklohrenz <joshua.lohrenz@gmail.com>
2014-06-04 16:54:32 -0700

    Implemented Pundit and basic roles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    authorize @posts
+
   end
 
   def show
@@ -10,11 +12,14 @@ class PostsController < ApplicationController
   end
 
   def new
-  @post = Post.new
+    @post = Post.new
+    authorize @post
   end
 
   def create
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    authorize @post
+
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -24,9 +29,23 @@ class PostsController < ApplicationController
       end
     end
 
-
-  def edit
+   def edit
     @post = Post.find(params[:id])
+    authorize @post
   end
+
+  def update
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+ 
 
 end
