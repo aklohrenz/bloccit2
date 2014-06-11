7b1729cc3efc0ff48bd1f7bf78b8e6419442efca
Aklohrenz <joshua.lohrenz@gmail.com>
2014-06-11 13:48:04 -0700

    Comments edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  respond_to :html, :js
+  
 
   def create
     @topic = Topic.find( params[:topic_id] )
@@ -17,10 +17,8 @@ class CommentsController < ApplicationController
     else
       flash[:error] = "There was an error saving the comment. Please try again."
     end
+    redirect_to [@topic, @post]
 
-    respond_with(@comment) do |f|
-      f.html { redirect_to [@topic, @post] }
-    end
   end
 
    private
